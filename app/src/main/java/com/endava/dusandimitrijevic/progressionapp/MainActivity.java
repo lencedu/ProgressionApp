5c7d3e832ad5be17f24929193a8348017956c0ad
end01230 <end01230@adobe.com>
2018-12-04 16:25:13 +0100

    [work] added listview for active teams, activity for new team

--- app/src/main/java/com/endava/dusandimitrijevic/progressionapp/MainActivity.java
+++ app/src/main/java/com/endava/dusandimitrijevic/progressionapp/MainActivity.java
@@ -1,13 +1,29 @@
 package com.endava.dusandimitrijevic.progressionapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String[] teams;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        teams = new String[] {"Arsenal", "Nice", "Inter"};
+        ArrayAdapter<String> teamsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, teams);
+        ListView teamsListView = (ListView) findViewById(R.id.teamsListView);
+        teamsListView.setAdapter(teamsAdapter);
+
+    }
+
+    public void addNewTeam(View view) {
+        Intent intent = new Intent(this, NewTeamActivity.class);
+        startActivity(intent);
     }
 }
